d300116ddf6125839391d8750e07c66427e0a11a
jacksonjj2-0 <91378063+jacksonjj2-0@users.noreply.github.com>
2024-05-06 16:49:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import threading
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import ChromeOptions
 
 
