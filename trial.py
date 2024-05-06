4f56709f4459f695a37dcb86b63825605fe491e0
jacksonjj2-0 <91378063+jacksonjj2-0@users.noreply.github.com>
2024-05-06 16:22:07 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -5,13 +5,13 @@ from selenium import webdriver
 # from selenium.webdriver import ChromeOptions
 
 
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--disable-gpu')
-# chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
 # chrome_options.add_argument("--window-size=1920,1080")
-# # driver = webdriver.Chrome(options=chrome_options)
+# driver = webdriver.Chrome(options=chrome_options)
 
 # # options = webdriver.ChromeOptions()
 # # options.add_argument("--incognito")
@@ -19,11 +19,12 @@ from selenium import webdriver
 
 # # driver = webdriver.Chrome(options=options)
 
-# options=ChromeOptions()
+options=chrome_options()
 # options.add_argument("--incognito")
 # options.add_argument("--headless")
 
-start_chrome("https://www.ecb.europa.eu/stats/financial_markets_and_interest_rates/euro_short-term_rate/html/index.en.html", headless=True)
+start_chrome("https://www.ecb.europa.eu/stats/financial_markets_and_interest_rates/euro_short-term_rate/html/index.en.html", options=chrome_options)
+
 
 
 # Click random point on the page to scroll down
