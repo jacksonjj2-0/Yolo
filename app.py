ad77236fdc4883507a87cef3d0fbd67adb2dae42
jacksonjj2-0 <91378063+jacksonjj2-0@users.noreply.github.com>
2024-05-06 16:47:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import ChromeOptions
 
 
-
+options = webdriver.ChromeOptions()
 options=ChromeOptions()
 options.add_argument("--incognito")
 options.add_argument("--headless")
