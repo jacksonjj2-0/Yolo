121a42f5ac86bd523102a1806bd705e7d4b7a4d4
jacksonjj2-0 <91378063+jacksonjj2-0@users.noreply.github.com>
2024-05-06 13:09:39 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta, timezone
+from helium import *
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ChromeOptions
+
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--disable-gpu')
+# chrome_options.add_argument('--disable-dev-shm-usage')
+# chrome_options.add_argument("--window-size=1920,1080")
+# driver = webdriver.Chrome(options=chrome_options)
+
+# options = webdriver.ChromeOptions()
+# options.add_argument("--incognito")
+# options.add_argument("--headless")
+
+# driver = webdriver.Chrome(options=options)
+
+options=ChromeOptions()
+options.add_argument("--incognito")
+options.add_argument("--headless")
+
+start_chrome("https://www.fbil.org.in/#/home", options=options)
+
+# Click random point on the page to scroll down
+
+hey = print("hello")
+# Scroll down to access table
+
+    
