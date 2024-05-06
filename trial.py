30a3faf641a5fb79fcc2c9e60439e50e0c79377b
jacksonjj2-0 <91378063+jacksonjj2-0@users.noreply.github.com>
2024-05-06 14:54:04 +0530

    Create trial.py

---
+++ trial.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta, timezone
+from helium import *
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ChromeOptions
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--window-size=1920,1080")
+driver = webdriver.Chrome(options=chrome_options)
+
+# options = webdriver.ChromeOptions()
+# options.add_argument("--incognito")
+# options.add_argument("--headless")
+
+# driver = webdriver.Chrome(options=options)
+
+options=ChromeOptions()
+options.add_argument("--incognito")
+options.add_argument("--headless")
+
+driver.get("https://www.ecb.europa.eu/stats/financial_markets_and_interest_rates/euro_short-term_rate/html/index.en.html")
+
+# Click random point on the page to scroll down
+
+hey = print("hello")
+# Scroll down to access table
